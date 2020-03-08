b6ee63e213a3af754a10625481d89694924a500f
xujiawei <xujiawei@zhizh.com>
2020-03-08 21:14:18 +0800

    第一次提交

--- src/main/java/com/yzy/DTO/ResultDTO.java
+++ src/main/java/com/yzy/DTO/ResultDTO.java
@@ -1,8 +1,17 @@
 package com.yzy.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
+/**
+ * @author xjw
+ */
 @Data
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResultDTO<T> {
     private Integer code;
     private String message;
--- src/main/java/com/yzy/utils/ResultDTOUtil.java
+++ src/main/java/com/yzy/utils/ResultDTOUtil.java
@@ -5,29 +5,25 @@ import com.yzy.DTO.ResultDTO;
 
 public class ResultDTOUtil {
 
-    public static ResultDTO SUCCESS(Object object){
-        ResultDTO resultDTO = new ResultDTO();
-        resultDTO.setCode(0);
-        resultDTO.setMessage("成功");
-        resultDTO.setDate(object);
-        return resultDTO;
+    public static ResultDTO SUCCESS(Object object) {
+        return new ResultDTO()
+                .setCode(0)
+                .setMessage("成功")
+                .setDate(object);
     }
 
-    public static ResultDTO SUCCESS(){
+    public static ResultDTO SUCCESS() {
         return SUCCESS(null);
     }
 
-    public static ResultDTO ERROR(Integer code, String message){
-        ResultDTO resultDTO = new ResultDTO();
-        resultDTO.setCode(code);
-        resultDTO.setMessage(message);
-        return resultDTO;
+    public static ResultDTO ERROR(Integer code, String message) {
+        return new ResultDTO()
+                .setCode(code)
+                .setMessage(message);
     }
 
-    public static ResultDTO ERROR(String message){
-        ResultDTO resultDTO = new ResultDTO();
-        resultDTO.setMessage(message);
-        return resultDTO;
+    public static ResultDTO ERROR(String message) {
+        return new ResultDTO().setMessage(message);
     }
 
 }
